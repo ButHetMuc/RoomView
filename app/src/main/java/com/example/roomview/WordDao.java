d9b9b02d72763f2c72478738c6cc6e389f8a6434
ButHetMuc <butle132@gmail.com>
2022-05-22 16:30:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/roomview/WordDao.java
@@ -0,0 +1,19 @@
+package com.example.roomview;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+    @Query("delete from word_table")
+    void deleteAll();
+    @Query("select * from word_table order by word ASC")
+   LiveData<List<Word>>  getAlphabetizedWords();
+}
